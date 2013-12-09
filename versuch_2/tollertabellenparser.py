3580f9e25bf31e1447c4ca67b802a8f08f45174a
Valentin Olpp <ox98aget@cip113.physik.uni-erlangen.de>
2013-12-09 19:04:36 +0100

    Python schreibe ich zuhause weiter

--- versuch_2/tollertabellenparser.py
+++ versuch_2/tollertabellenparser.py
@@ -4,5 +4,15 @@ fname=input("Wie heißt die Datei?")
 f=open(fname)
 fstr=f.read()
 flist=fstr.splitlines()
+roundlist=[]
 for eintrag in flist:
-	a=eintrag.split()[0] 
+	a=eintrag.split()[0]
+    ar=a.split(,)[0]
+    b=eintrag.split()[1]
+    bp=b.replace(",",".")
+    br=round(bp,2)
+    eintragb=[ar,br]
+    eintragedit=eintragb.join("\t&\t")
+    roundlist.append(eintragedit+"\\\\\n")
+
+
