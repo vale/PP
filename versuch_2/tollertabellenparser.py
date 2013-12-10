9265d88fbfdcfc2dbae9d3b9c05cd1608fcedd3e
vale <vale@git.olpp.net>
2013-12-10 08:13:01 +0100

    Änderungen Tabelle im Anhang

--- versuch_2/tollertabellenparser.py
+++ versuch_2/tollertabellenparser.py
@@ -1,18 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
 
-fname=input("Wie heißt die Datei?")
+fname=raw_input("Wie heisst die Datei?")
+foutname=raw_input("Outputdatei?")
 f=open(fname)
 fstr=f.read()
+f.close()
 flist=fstr.splitlines()
-roundlist=[]
+flist=flist[::20]
+roundlist=""
 for eintrag in flist:
-	a=eintrag.split()[0]
-    ar=a.split(,)[0]
+    a=eintrag.split()[0]
+    a=a.split(",")[0]
     b=eintrag.split()[1]
-    bp=b.replace(",",".")
-    br=round(bp,2)
-    eintragb=[ar,br]
-    eintragedit=eintragb.join("\t&\t")
-    roundlist.append(eintragedit+"\\\\\n")
-
-
+    b=b.replace(",",".")
+    b="%.2f"%float(b)
+    b=b.replace(".",",")
+    eintragb=a+"\t&\t"+b
+    roundlist=roundlist+eintragb+"\\\\\n"
+fout=open(foutname, "w")
+fout.write(roundlist)
+fout.close()
